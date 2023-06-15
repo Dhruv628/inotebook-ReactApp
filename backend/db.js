65cc5b9f4bca83e06bea914ae1bc9a80f67bed1d
Dhruv628 <115400138+Dhruv628@users.noreply.github.com>
2023-06-15 10:57:29 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURL =`mongodb+srv://sharmaaniketpubg:2eaBL274BwmecrZE@cluster0.zeewoam.mongodb.net/?retryWrites=true&w=majority`
+const mongoURL ="mongodb+srv://sharmaaniketpubg:2eaBL274BwmecrZE@cluster0.zeewoam.mongodb.net/?retryWrites=true&w=majority"
 const hi='hi'
 async function connectToMongo() {
     mongoose.connect(mongoURL).then(
